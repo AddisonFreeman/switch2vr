62242aae9b1e35488ec86bd8d3f530182d3a1206
addisonfreeman <afree1423@gmail.com>
2017-08-22 12:47:37 -0500

    add drag & drop support for directories in admin screen, begin js file validation

--- admin/js/switch2vr-admin.js
+++ admin/js/switch2vr-admin.js
@@ -29,4 +29,56 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
+	document.addEventListener('DOMContentLoaded', function(event) {
+		if (window.File && window.FileReader && window.FileList && window.Blob) {
+		  // Great success! All the File APIs are supported.
+		} else {
+		  alert('The File APIs are not fully supported in this browser.');
+		}
+
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload) {
+			//XHR Supported
+		} else {
+			alert('XHR HTTP Requests not supported by your browser');
+		}
+
+		// var dropDiv = document.getElementById('dropDiv');
+
+		// dropDiv.addEventListener('dragover', function(event) {
+		// 	event.preventDefault();
+		// });
+
+		// dropDiv.addEventListener('drop', function (e) {
+		// 	e.stopPropagation();
+		// 	e.preventDefault();
+		// 	// var files = e.target.files || e.dataTransfer.files;
+		// 	console.log(e.dataTransfer.files);
+		// });
+		
+		var filesEl = document.getElementById('files');
+
+		filesEl.addEventListener('dragover', function(event) {
+			event.preventDefault();
+			console.log("dragging");
+		});
+
+		filesEl.addEventListener('change', function (e) {
+			e.stopPropagation();
+			e.preventDefault();
+
+			// console.log(e.target.files);
+
+			var files = e.target.files;
+			for (var i = 0, file; file = files[i]; i++) {
+				console.log(file);
+			}
+
+			var uploadFile = function(file, path) {
+				console.log(path, file);
+				// handle file uploading
+			};
+		});
+	});
+
 })( jQuery );
